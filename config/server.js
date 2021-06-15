e13337bfe7343f69c9116a15dec522e2ddd2cfe5
Arup Das <arupdas872@gmail.com>
2021-06-15 19:02:41 +0530

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9456848387ed308e9c12b29ca34d2289'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7f227f7d-7f52-4b7c-a3b6-de894a734610'
+};
